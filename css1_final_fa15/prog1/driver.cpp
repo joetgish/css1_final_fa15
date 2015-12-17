dce00942427d335f14730c2c0b1483cb7847cba2
Fred Flinstone <fflinstone@gmail.com>
2015-12-17 15:05:17 -0800

    Exam file posting

---
+++ css1_final_fa15/prog1/driver.cpp
@@ -0,0 +1,26 @@
+// Name
+// Date
+// CSS-1 Fa15 Final Exam Prog 1
+#include <iostream>
+using namespace std;
+
+void fillVector(vector<string>& PW, ifstream& A_in);
+
+int main()
+{
+	const int SIZE = 6;
+	string possible[SIZE] = {"zymogenic","aboral","Santa","password1","Password!",
+							  "ambystomidae"};
+
+	return 0;
+}
+
+
+//*************************************
+void fillVector(vector<string>& PW, ifstream& A_in)
+{
+	
+}// end fill Vector(_)
+
+
+ 
---
+++ css1_final_fa15/prog2/driver.cpp
@@ -0,0 +1,27 @@
+// Program comments here 
+// 
+// 
+#include <iostream>
+using namespace std;
+
+const int R = 30;
+const int C = 50;
+ 
+void fillArray(ifstream& A_in, string S[][C]);
+
+int main()
+{
+	
+	return 0;
+}
+
+// ********************************
+void fillArray(ifstream& A_in, string S[][C])
+{
+	
+}//end fillArray
+
+
+
+
+ 
