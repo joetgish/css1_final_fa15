eef435e9cd8e9d1eeb8584a96f8f5a943d532010
Noe Lomeli <noelomeli@student.hartnell.edu>
2015-12-17 17:41:48 -0800

    Final test prog1 complete

--- css1_final_fa15/prog1/driver.cpp
+++ css1_final_fa15/prog1/driver.cpp
@@ -2,9 +2,15 @@
 // Date
 // CSS-1 Fa15 Final Exam Prog 1
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
+bool isGoodPassWord(ofstream& A_out, vector<string>& PW, string arryA[], int n);
 void fillVector(vector<string>& PW, ifstream& A_in);
+// Precondition: Ifstream file and  string vector are to be input into the function
+//Postcondition: Vector will be populated with strings
+//********************************************************************************
 
 int main()
 {
@@ -12,15 +18,75 @@ int main()
 	string possible[SIZE] = {"zymogenic","aboral","Santa","password1","Password!",
 							  "ambystomidae"};
 
+	vector<string> M;
+	ifstream fin;
+	ofstream fout;
+	fout.open("good_words.txt");
+	fin.open("passwords.txt");
+	if(fin.fail())
+	{
+		cout << "Error opening file" << endl;
+	}
+	if(fout.fail())
+	{
+		cout << "Error opening file" << endl;
+	}
+
+	fillVector(M, fin);
+	fout << "Noe Lomeli" << endl;
+	isGoodPassWord(fout, M, possible, SIZE);
+
+
+	fin.close();
+	fout.close();
 	return 0;
-}
+} // end main
 
 
 //*************************************
 void fillVector(vector<string>& PW, ifstream& A_in)
 {
-	
+	string word;
+	while(A_in >> word)
+	{
+		PW.push_back(word);
+		
+	}
 }// end fill Vector(_)
+//************************************************
+bool isGoodPassWord(ofstream& A_out, vector<string>& PW, string arryA[], int n)
+{
+	bool isNotHere = true;
+	
+	
+	for(int i(0); i < n; i++)
+	{
+		for(int j(0); j < PW.size(); j++)
+		{		
+			if(arryA[i] == PW[j])
+			{
+				isNotHere = true;
+				break;
+				
+
+			}
+			else
+			{
+				isNotHere = false;
+				
+			}
+			
+		}
+		if(isNotHere != true)
+		{
+			A_out << arryA[i] << endl;
+		}
+		
+	}
+
+}
+
+
 
 
  
