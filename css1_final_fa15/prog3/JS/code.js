d17af1365f6636348bea49b889a2418e070904e7
CarlosHuizarValenzuela <carloshuizar@student.hartnell.edu>
2015-12-17 18:28:04 -0800

    Compleated code

---
+++ css1_final_fa15/prog3/JS/code.js
@@ -0,0 +1,18 @@
+// Name: Carlos Huizar Valenzuela 
+// Date: 17 Dec 2015	
+// Prog 3
+
+
+document.write("12 days to Chrismas<br>");
+document.write("11 days to Chrismas<br>");
+document.write("10 days to Chrismas<br>");
+document.write("9 days to Chrismas<br>");
+document.write("8 days to Chrismas<br>");
+document.write("7 days to Chrismas<br>");
+document.write("6 days to Chrismas<br>");
+document.write("5 days to Chrismas<br>");
+document.write("4 days to Chrismas<br>");
+document.write("3 days to Chrismas<br>");
+document.write("2 days to Chrismas<br>");
+document.write("1 days to Chrismas<br>");
+document.write("Chrismas is here<br>");
