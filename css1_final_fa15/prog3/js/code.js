8e5416212b6a8ab26ab9c50f4484483505b4c159
Nicole Hipolito <nicolebernadetteihipolito@student.hartnell.edu>
2015-12-17 18:00:32 -0800

    Made prog 3

--- css1_final_fa15/prog3/js/code.js
+++ css1_final_fa15/prog3/js/code.js
@@ -1,6 +1,6 @@
-document.write("Nicole Hipolito");
-for(int i = 12; i > 0; i--){
-	document.write(i + " days of Christmas");
-}
+	document.write("Nicole Hipolito\n");
+	for(int i = 12; i > 0; i--){
+		document.write(i + " days of Christmas\n");
+	}
 
-document.write("Christmas is here!!!");
+	document.write("Christmas is here!!!\n");
