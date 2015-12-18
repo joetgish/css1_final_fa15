71d6eb3dcc13bab5f33f27f87247f5803707c64d
Louis Paul Romero <louispromero@student.hartnell.edu>
2015-12-17 17:11:19 -0800

    JS Program Complete

---
+++ css1_final_fa15/parent/JS/program.js
@@ -0,0 +1,12 @@
+function christmasCountdown()
+{
+	for(var ix = 12; ix > 0; ix--)
+	{
+		document.write(ix," days of Christmas");
+		document.write("<br>");
+	}
+}
+
+christmasCountdown();
+document.write("Christmas is here!!");
+
