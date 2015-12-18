d93e98de1fcbcba1c3ac7ba522734613528960f6
NancyGomez <nancyhgomez@student.hartnell.edu>
2015-12-17 17:12:34 -0800

    Finished all programs

---
+++ css1_final_fa15/prog3/js/program.js
@@ -0,0 +1,9 @@
+document.write("Nancy Gomez<br>");
+for (var i = 12; i >= 0; i--){
+	if (i !== 0){
+		document.write(i + " days of Christmas<br>");
+	}
+	else{
+		document.write("Christmas is here!!");
+	}
+}
