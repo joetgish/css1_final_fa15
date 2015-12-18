34d624ddfa303d6152290e40916e0e991f00459f
Samuel Sanchez <samueljsanchez@student.hartnell.edu>
2015-12-17 17:36:48 -0800

    Prog3 contains 3 directories and 1 file. The 3 directories are CSS, images, and js. The file is the html file index.html. Inside the js directory contains the javascript file "code.js" The code contains a function that prints a countdown from 12 with the following "x days of Christmas" The top contains a line that prints the author's name, and the bottom of the function contains a line that prints "Christmas is here!!" All of which is printed to the index.html file in line break fasion.

--- css1_final_fa15/prog3/js/code.js
+++ css1_final_fa15/prog3/js/code.js
@@ -2,15 +2,15 @@
 // Date: 17 December 2015
 // CSS-1 Fa15 Final Exam Prog 3
 
-function xmasPrint() // Prints out 12 days of christmas
+function xmasPrint() // Prints out 12 days of christmas (start)
 {
-	document.write("Samuel Sanchez\t");
+	document.write("Samuel Sanchez");
 	var daysTill = 12; // Countdown of days
 	for(var xi = daysTill; xi > 0; xi--)
 	{
-		document.write(xi + " days of Christmas\n");
+		document.write("<p>" + xi + " days of Christmas</p>"); // <p> added for line skipping
 	}
-	document.write("Christmas is here!!\n");
-}
+	document.write("Christmas is here!!"); 
+} // End of function
 
 xmasPrint();
