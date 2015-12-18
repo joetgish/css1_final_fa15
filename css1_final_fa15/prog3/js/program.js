c2d291492b8fee68f5612ffc200334cedc370439
Bianca Hernandez <biancakhernandez.student.hartnell.edu>
2015-12-17 18:42:11 -0800

    Implement programs 1-3 for Final css1 Exam

---
+++ css1_final_fa15/prog3/js/program.js
@@ -0,0 +1,11 @@
+// Bianca Hernandez
+// Date: December 17, 2015
+// CSS-1 Fa15 Final Exam Prog 3
+//**********************************************
+
+console.log();
+for(var i = 12; i > 0; i--)
+{
+	document.write(i + " days of Christmas <br>"); //<br> is to start a newline
+}
+document.write("Christmas is here!!");
