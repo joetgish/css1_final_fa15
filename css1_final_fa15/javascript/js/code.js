891f310e8d0592ac2090c66fe56a87b3d8efb40b
Nicholas Rosas <nicholasirosas@student.hartnell.edu>
2015-12-17 18:19:44 -0800

    CSS 1 Final Exam

---
+++ css1_final_fa15/javascript/js/code.js
@@ -0,0 +1,14 @@
+function christmas()
+{
+	document.write("Nicholas Rosas");
+	document.write("<br>");
+	document.write("<br>");
+	for(var ix = 12; ix > 0; ix--)
+	{
+		document.write(ix + "\ days of Christmas<br>");
+		document.write("<br>");
+	}
+	document.write("Christmas is here!!<br>");
+}
+
+christmas();
