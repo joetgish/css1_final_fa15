b0081b8b4e6ba4080a331114d55f60c87559f5be
EmilyGarcia <emilygarcia@student.hartnell.edu>
2015-12-17 18:04:29 -0800

    Completed Code

---
+++ css1_final_fa15/prog3/javaScript/christmas.js
@@ -0,0 +1,14 @@
+document.write("Emily Garcia</br>");
+
+for(var i = 12; i >= 0; i--)
+{
+	if(i != 0)
+	{
+		document.write("\n " + i + " days of Christmas</br>");
+	}
+	else
+	{
+		document.write("Christmas is here!!</br>");
+	}
+
+}
